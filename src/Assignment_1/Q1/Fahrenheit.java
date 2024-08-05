b1b7e87999634c0647830693707c624073b0c0f5
ujan <ujan.shakya@gurzu.com>
2024-08-05 22:45:53 +0545

    minor fix

--- src/Assignment_1/Q1/Fahrenheit.java
+++ src/Assignment_1/Q1/Fahrenheit.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Fahrenheit {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter Temperature in Fahrenheit: ");
+        System.out.println("Enter Temperature in Fahrenheit is :  ");
         double temp = sc.nextDouble();
         double celsius = (temp - 32) * 5 / 9;
         System.out.println("The Celsius is " + celsius);
